573b129c071a9dbd02e7dd6bb8f301a6389b9ae5
sebemmrob <sebastian241007@icloud.com>
2025-10-29 02:03:28 +0000

    added bogo_sort

--- visualised_sort.py
+++ visualised_sort.py
@@ -8,8 +8,9 @@
 import random 
 import time
 import os
-def gen_num_list():
-    arr1 = list(range(1,26))
+
+def gen_num_list(sizex):
+    arr1 = list(range(1,sizex))
     random.shuffle(arr1)
     return(arr1)
 
@@ -36,25 +37,68 @@ def bubble_sort(bubble_list):
             os.system("cls")
 
             visual_sort = visualize(bubble_list)
-            for k in range(len(visual_sort)-1):
+            for k in range(len(visual_sort)):
                 print(visual_sort[k])
 
-            time.sleep(0.05)
+            time.sleep(0.03)
 
         if sorted_check == True:
             sorted = True
-
+    print(bubble_list)
     return bubble_list
+
+def bogo_sort(bogo_list,sizex):
+    sorted = False
+    while sorted == False:
+        temparr = []
+        for i in range(len(bogo_list)):
+            temparr.append(1)
+        sorted_check = True
+
+        places = list(range(0,sizex-1))
+        random.shuffle(places)
+        #print("places",str(places)) debug
+
+
+        for i in range(0,len(bogo_list)):
+            #print("i",str(i)) debug
+            #print("temparr",temparr) debug
+            #print("bogo_list",bogo_list) debug
+
+            temparr[places[i]] = bogo_list[i]
+        for j in range(len(temparr)-1):
+            if temparr[j] > temparr[j+1]:
+                sorted_check = False
+        if sorted_check == True:
+            sorted = True
+
+        os.system("cls")
+        
+        visual_sort = visualize(temparr)
+        for k in range(len(visual_sort)):
+            print(visual_sort[k])
+
+        time.sleep(0.03)
+    print(temparr)
+    return(temparr)
 
 def visualize(visual_list):
     arr2 = []
-    for i in range(len(visual_list)-1):
+    for i in range(len(visual_list)):
         length1 = visual_list[i] 
         hashtags = "#" *length1
         arr2.append(hashtags)
     return arr2
 
+userinp = int(input("input the length of the list you wish to sort: "))
+size = userinp + 1
 
-sort_inp = gen_num_list()
+
+sort_inp = gen_num_list(size)
 print(sort_inp)
-print(bubble_sort(sort_inp))
+bubble_out = bubble_sort(sort_inp)
+time.sleep(5)
+bogo_out = bogo_sort(sort_inp,size)
+
+print("bubble sort output",bubble_out)
+print("bogo sort output",bogo_out)
